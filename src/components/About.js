eb03f88ee2c176c92938f3fb14bb6d9f02b25e3e
James-Kimanzi <jameskimanzi@gmail.com>
2024-03-14 16:39:28 +0300

    completed assignment

--- src/components/About.js
+++ src/components/About.js
@@ -2,19 +2,15 @@ import React from "react";
 import Links from "./Links";
 
 function About(props) {
-  const { bio, github, linkedin } = props;
-
   return (
     <div id="about">
       <h2>About Me</h2>
-      {bio && bio !== '' && <p>{bio}</p>}
+        {props.bio && props.bio.length > 1 ? <p>{props.bio}</p> : null}
+  
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
-      <Links 
-        github={github} 
-        linkedin={linkedin}
-      />
+      {/* add your <Links /> component here */}
+      <Links github={props.links.github} linkedin={props.links.linkedin} />
     </div>
   );
 }
-
 export default About;
--- src/components/App.js
+++ src/components/App.js
@@ -2,22 +2,17 @@ import React from "react";
 import NavBar from "./NavBar";
 import Home from "./Home";
 import About from "./About";
+
+// pass this data down as props to the child component(s) that need it!
 import user from "../data/user";
+console.log(user);
 
 function App() {
   return (
     <div>
       <NavBar />
-      <Home 
-        color={user.color}
-        name={user.name}
-        city={user.city}
-      />
-      <About
-        bio={user.bio}
-        github={user.links.github} 
-        linkedin={user.links.linkedin} 
-      />
+      <Home color ={user.color} name={user.name} city ={user.city} />
+      <About bio={user.bio} links={user.links}/>
     </div>
   );
 }
