c8c3508fe082e27afcaa70b09031a074762cc21d
James-Kimanzi <jameskimanzi@gmail.com>
2024-03-14 16:23:05 +0300

    completed assignment

--- src/components/About.js
+++ src/components/About.js
@@ -2,15 +2,16 @@ import React from "react";
 import Links from "./Links";
 
 function About(props) {
-  const { bio, links } = props;
+  const { bio, github, linkedin } = props;
+
   return (
     <div id="about">
       <h2>About Me</h2>
       {bio && bio !== '' && <p>{bio}</p>}
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
       <Links 
-        github={links.github} 
-        linkedin={links.linkedin}
+        github={github} 
+        linkedin={linkedin}
       />
     </div>
   );
